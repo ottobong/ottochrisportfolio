ecbc89bb49497cbb74832cf11ca23ac34e99270d
Ottobong <clemency50@gmail.com>
2022-09-10 01:37:06 +0100

    added new lines of code to css

---
+++ script.js
@@ -0,0 +1,11 @@
+const header = document.querySelector("header");
+
+// Sticky Navbar
+
+function stickyNavbar() {
+    header.classList.toggle("scrolled", window.pageYOffset > 0);
+}
+
+stickyNavbar();
+
+window.addEventListener("scroll", stickyNavbar);
