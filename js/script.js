1b4c234735e707766936420aa55421e0509351d5
Ottobong <clemency50@gmail.com>
2022-09-16 18:49:16 +0100

    added new lines of code

--- js/script.js
+++ js/script.js
@@ -127,20 +127,25 @@ activeLink();
 
 
 //Dark and Light mode toggle
+let firstTheme = localStorage.getItem("dark");
 
-function changeTheme() {
-    if(!document.body.classList.contains("dark")) {
+changeTheme(+firstTheme);
+
+function changeTheme(isDark) {
+    if(isDark) {
         document.body.classList.add("dark");
         toggle_btn.classList.replace("uil-moon", "uil-sun");
+        localStorage.setItem("dark", 1);
     }
     else {
         document.body.classList.remove("dark");
         toggle_btn.classList.replace("uil-sun", "uil-moon");
+        localStorage.setItem("dark", 0)
     }
 }
 
 toggle_btn.addEventListener("click", () => {
-    changeTheme();
+    changeTheme(!document.body.classList.contains("dark"));
 });
 
 
