e3b9ffd5d166f8fb5c1d4492ff0fd0ab38fd1343
Ottobong <clemency50@gmail.com>
2022-09-18 15:24:04 +0100

    added new line

--- js/script.js
+++ js/script.js
@@ -155,4 +155,13 @@ toggle_btn.addEventListener("click", () => {
 
 hamburger.addEventListener("click", () => {
     document.body.classList.toggle("open");
-})
+    document.body.classList.toggle("stopScrolling");
+});
+
+
+links.forEach((link) =>
+    link.addEventListener("click", () => {
+        document.body.classList.remove("open");
+        document.body.classList.remove("stopScrolling");
+    })
+)
