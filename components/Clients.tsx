cdf49aa4ae34f83aeae107d392003bf3287d8dc7
Ottobong <clemency50@gmail.com>
2024-09-05 19:07:23 +0100

    updated pages

--- components/Clients.tsx
+++ components/Clients.tsx
@@ -32,14 +32,14 @@ const Clients = () => {
                 <img
                   src={company.img}
                   alt={company.name}
-                  className="md:w-10 w-5"
+                  className="md:w-32 w-8"
                 />
-                <img
+                {/* <img
                   src={company.nameImg}
                   alt={company.name}
                   width={company.id === 4 || company.id === 5 ? 100 : 150}
                   className="md:w-24 w-20"
-                />
+                /> */}
               </div>
             </React.Fragment>
           ))}
